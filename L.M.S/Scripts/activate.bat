5417f1e049b452da79832d5d65a3ca26546a7acd
Seif-moha <saifmhsa2030@gmail.com>
2023-12-07 00:16:31 +0300

    change my last folder

--- L.M.S/Scripts/activate.bat
+++ L.M.S/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\MHSAF\Desktop\Django\DjangoBoilerplate\DjangoBoilerplate\lms
+set VIRTUAL_ENV=C:\Users\MHSAF\Desktop\Django\L.M.S
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(lms) %PROMPT%
+set PROMPT=(L.M.S) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(lms) 
+set VIRTUAL_ENV_PROMPT=(L.M.S) 
 
 :END
 if defined _OLD_CODEPAGE (
--- L.M.S/project/lms_app/views.py
+++ L.M.S/project/lms_app/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from .models import *
+from .forms import *
 # Create your views here.
+
+def index(request):
+
+    if request.method == 'POST':
+        add_book = BookForm(request.POST, request.FILES)
+        if add_book.is_valid():
+            add_book.save()
+
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
+    context = {
+        'categories': Category.objects.all(),
+        'books': Book.objects.all(),
+        'form': BookForm(),
+        'formcategory': CategoryForm(),
+    }
+
+    return render(request, 'pages/index.html', context)
+
+def books(request):
+
+    context = {
+        'categories': Category.objects.all(),
+        'books': Book.objects.all(),
+    }
+
+    return render(request, 'pages/books.html', context)
