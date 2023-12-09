419fa8e6f18887ec6b8be6a77b9b53a626102f4d
Seif-moha <saifmhsa2030@gmail.com>
2023-12-09 20:39:42 +0300

    i added some changes in my project

--- L.M.S/project/lms_app/views.py
+++ L.M.S/project/lms_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
 # Create your views here.
@@ -31,3 +31,25 @@ def books(request):
     }
 
     return render(request, 'pages/books.html', context)
+
+def update(request, id):
+    book_id = Book.objects.get(id=id)
+    if request.method == 'POST':
+        book_save = BookForm(request.POST, request.FILES, instance=book_id)
+        if book_save.is_valid():
+            book_save.save()
+            return redirect('/')
+    else:
+        book_save = BookForm(instance=book_id)
+
+    context = {
+        'form': book_save,
+    }
+    return render(request, 'pages/update.html', context)
+
+def delete(request, id):
+    book_delete = get_object_or_404(Book, id=id)
+    if request.method == 'POST':
+        book_delete.delete()
+        return redirect('/')
+    return render(request, 'pages/delete.html')
